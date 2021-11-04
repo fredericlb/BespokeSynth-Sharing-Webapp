07643a8f016c4ffea7700d19b604e50e3c7a0ed4
Frédéric Langlade-Bellone <fred@parapluie.org>
2021-11-04 08:31:35 +0100

    fix: removed useless dagre dependency

--- bespoke-patches-client/src/components/BSKViz.tsx
+++ bespoke-patches-client/src/components/BSKViz.tsx
@@ -1,4 +1,3 @@
-import dagre from "dagre";
 import React, { useCallback, useMemo, useState } from "react";
 import ReactFlow, {
   Background,
@@ -8,48 +7,12 @@ import ReactFlow, {
   Elements,
   isNode,
   Node,
-  Position,
 } from "react-flow-renderer";
 import { Module } from "../hooks/patch.types";
 import TextModal from "./TextModal";
 
-const dagreGraph = new dagre.graphlib.Graph();
-dagreGraph.setDefaultEdgeLabel(() => ({}));
-
 const nodeWidth = 172;
 const nodeHeight = 36;
-
-const getLayoutedElements = (elements: Elements) => {
-  dagreGraph.setGraph({ rankdir: "TB" });
-
-  elements.forEach((el) => {
-    if (isNode(el)) {
-      dagreGraph.setNode(el.id, { width: nodeWidth, height: nodeHeight });
-    } else {
-      dagreGraph.setEdge(el.source, el.target);
-    }
-  });
-
-  dagre.layout(dagreGraph);
-
-  return elements.map((el) => {
-    if (isNode(el)) {
-      const nodeWithPosition = dagreGraph.node(el.id);
-      el.targetPosition = Position.Top;
-      el.sourcePosition = Position.Bottom;
-
-      // unfortunately we need this little hack to pass a slightly different position
-      // to notify react flow about the change. Moreover we are shifting the dagre node position
-      // (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
-      el.position = {
-        x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
-        y: nodeWithPosition.y - nodeHeight / 2,
-      };
-    }
-
-    return el;
-  });
-};
 
 const BSKViz: React.FC<{ modules: Module[] }> = ({ modules }) => {
   const [modalContent, setModalContent] =
