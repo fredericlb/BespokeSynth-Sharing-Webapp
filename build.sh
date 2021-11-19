d399169e555fc6fef20f9e057baf0a5fb0c92494
Frédéric Langlade-Bellone <fred@parapluie.org>
2021-11-19 16:16:41 +0100

    fix: bad build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e -u
 
 rm -rf ./bespoke-patches-server/public/client
-#cd bespoke-patches-server && npm install
-#cd ..
-cd bespoke-patches-client  && npm run build
+cd bespoke-patches-server && npm install
+cd ..
+cd bespoke-patches-client  && npm install && npm run build
 cd ..
 cp -rf bespoke-patches-client/build bespoke-patches-server/public/client
