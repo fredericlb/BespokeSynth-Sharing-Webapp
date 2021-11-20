4f334dac10e49656452e386cb3830771195aeea1
Frédéric Langlade-Bellone <fred@parapluie.org>
2021-11-20 07:57:50 +0100

    fix(syntax-highlighter): load language

--- bespoke-patches-client/src/components/SyntaxHighlighter.tsx
+++ bespoke-patches-client/src/components/SyntaxHighlighter.tsx
@@ -4,8 +4,8 @@ import markdown from "react-syntax-highlighter/dist/esm/languages/hljs/markdown"
 import python from "react-syntax-highlighter/dist/esm/languages/hljs/python";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-SyntaxHighlighterCore.registerLanguage(python, "python");
-SyntaxHighlighterCore.registerLanguage(markdown, "markdown");
+SyntaxHighlighterCore.registerLanguage("python", python);
+SyntaxHighlighterCore.registerLanguage("markdown", markdown);
 
 const SyntaxHighlighter: React.FC<{ language: "python" | "markdown" }> = ({
   language,
